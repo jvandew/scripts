3c61b6dd41bf9410b66f3f83d30b968e9cbf7efe
Jacob Van De Weert <vandeweertj@gmail.com>
2014-07-15 11:49:33 -0400

    fix bug in musicRenamer when running in a separate directory

--- musicRenamer.py
+++ musicRenamer.py
@@ -1,4 +1,6 @@
-import argparse, os
+import argparse
+from os import listdir, rename
+from os.path import join
 
 parser = argparse.ArgumentParser(description='This script removes annoying track numbers and whatnot from the names of music files in the given directory')
 parser.add_argument('music_dir', help='Directory containing music files')
@@ -7,9 +9,9 @@ parser.add_argument('-ft', default='mp3', dest='file_type', help='Extension of f
 
 args = parser.parse_args()
 
-for filename in os.listdir(args.music_dir):
+for filename in listdir(args.music_dir):
   if filename.endswith(args.file_type):
     new_name = filename[args.drop_chars:]
-    print('Renaming \'' + filename + '\' to \'' + new_name + '\'.')
-    os.rename(filename, new_name)
+    print 'Renaming \'%s\' to \'%s\'.' % (filename, new_name)
+    rename(join(args.music_dir, filename), join(args.music_dir, new_name))
 
