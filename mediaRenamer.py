617d7b6eeae1602a3e4144755d8df9eebea19b1f
Jacob Van De Weert <vandeweertj@gmail.com>
2014-11-29 15:44:39 -0500

    rename musicRenamer and add replace option

--- mediaRenamer.py
+++ mediaRenamer.py
@@ -5,13 +5,14 @@ from os.path import join
 parser = argparse.ArgumentParser(description='This script removes annoying track numbers and whatnot from the names of music files in the given directory')
 parser.add_argument('music_dir', help='Directory containing music files')
 parser.add_argument('drop_chars', type=int, help='Number of characters to drop from the filename (eventually this will be replaced with a regex)')
-parser.add_argument('-ft', default='mp3', dest='file_type', help='Extension of files to target. Default is \'mp3\'.')
+parser.add_argument('-replace', default='', help='Optionally replace dropped characters with a new prefix')
+parser.add_argument('-ft', default='mp3', dest='file_type', help='Extension of files to target. Default is \'mp3\'')
 
 args = parser.parse_args()
 
 for filename in listdir(args.music_dir):
   if filename.endswith(args.file_type):
-    new_name = filename[args.drop_chars:]
+    new_name = '{0}{1}'.format(args.replace, filename[args.drop_chars:])
     print 'Renaming \'%s\' to \'%s\'.' % (filename, new_name)
     rename(join(args.music_dir, filename), join(args.music_dir, new_name))
 
